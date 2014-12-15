760d2331bb2e1f78594a9edfa9c2c64d399fc4de
Vlad.Vul <xoid@ya.ru>
2014-12-15 03:42:33 -0500

    FastTemplate is better

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -1,12 +1,35 @@
 #!/usr/bin/perl
 
 use CGI;
-use CGI::Template;
+use CGI::FastTemplate;
 use strict;
 
 print "Content-type: text/html\n\n";
 
 print "Hello world\n";
 
-my $tmpl = new CGI::Template;
+CGI::FastTemplate->set_root('tmpl/');
+my $tpl = new CGI::FastTemplate('list.tmpl');
+
+$tpl->define( main    => "main.tpl",
+                row     => "table_row.tpl",
+                all     => "table_all.tpl",
+                );
+
+$tpl->assign(TITLE => "I am the title.");
+
+my %defaults = (    #FONT   => "<font size=+2 face=helvetica>",
+                    EMAIL   => 'jmoore@sober.com',
+                    );   
+$tpl->assign(\%defaults);
+
+  $tpl->parse(ROWS      => ".row");      ## the '.' appends to ROWS
+  $tpl->parse(CONTENT   => ["row", "all"]);
+  $tpl->parse(CONTENT   => "main");
+
+  $tpl->print();            ## defaults to last parsed
+  $tpl->print("CONTENT");   ## same as print() as "CONTENT" was last parsed
+
+  $ref = $tpl->fetch("CONTENT");        
+
 
